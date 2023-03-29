00acec39518f82f22436991aa6b8cb5e38214810
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-29 08:57:45 +0530

    Adding firstName

---
+++ src/com/bridgeLabz/UserRegestration.java
@@ -0,0 +1,23 @@
+package com.bridgeLabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegestration {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter Fist name:- ");
+	        String name = sc.next();
+	        System.out.print("Enter Last name:- ");
+	        String lastname = sc.next();
+	        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
+	        Matcher matcher = pattern.matcher(name);
+	        Matcher matcher1 = pattern.matcher(lastname);
+	        if (matcher.matches()&& matcher1.matches())
+	            System.out.println("valid");
+	        else
+	            System.out.println("Invalid");
+	    }
+	}
+
