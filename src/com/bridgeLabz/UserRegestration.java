cedfc3f4bb72458bd157a9a186b32c190717177f
Vamshi77reddy <vamshireddy6563@gmail.com>
2023-03-29 10:43:31 +0530

    Added Numericvalue

--- src/com/bridgeLabz/UserRegestration.java
+++ src/com/bridgeLabz/UserRegestration.java
@@ -9,7 +9,7 @@ public class UserRegestration {
 	        Scanner sc = new Scanner(System.in);
 	        System.out.print("Enter Fist name:- ");
 	        String name = sc.next();
-	        System.out.print("Enter Fist name:- ");
+	        System.out.print("Enter LastFist name:- ");
 	        String lastName = sc.next();
 	        
 	        Pattern pattern = Pattern.compile("^[A-Z][a-z]{3,}$");
@@ -22,9 +22,9 @@ public class UserRegestration {
 	            System.out.println("Invalid");
 	        System.out.print("Enter Email id:- ");
 	        String Email = sc.next();
-	        Pattern pattern3 = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
-	        Matcher matcher3 = pattern3.matcher(Email);
-	        if(matcher3.matches()){
+	        Pattern pattern2 = Pattern.compile("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+	        Matcher matcher2 = pattern2.matcher(Email);
+	        if(matcher2.matches()){
 	            System.out.println("valid");
 	        }
 	        else {
@@ -32,9 +32,9 @@ public class UserRegestration {
 	        }
 	        System.out.print("Enter Mobile number:- ");
 	        String mobileNumber = sc.next();
-	        Pattern pattern4 = Pattern.compile("^[7-9][0-9]{9}$");
-	        Matcher matcher4 = pattern4.matcher(mobileNumber);
-	        if(matcher4.matches()){
+	        Pattern pattern3 = Pattern.compile("^[7-9][0-9]{9}$");
+	        Matcher matcher3= pattern3.matcher(mobileNumber);
+	        if(matcher3.matches()){
 	            System.out.println("valid");
 	        }
 	        else {
@@ -42,14 +42,17 @@ public class UserRegestration {
 	        }
 	        System.out.print("Enter the Password:- ");
 	        String Password = sc.next();
-	        Pattern pattern6 = Pattern.compile("^[A-Za-z]{8,}$");
-	        Matcher matcher6 = pattern.matcher(Password);
-	        if (matcher.matches())
+	        Pattern pattern4 = Pattern.compile("^[A-Z][a-z]{1,7}[0-9]{1}$");
+	        Matcher matcher4 = pattern4.matcher(Password);
+	        if (matcher4.matches())
 	            System.out.println("valid password");
-	        else
-	            System.out.println("Invalid password)"); 
+	        else {
+	            System.out.println("Invalid password");
 	        System.out.println("Minimum 8 Characters");
 			System.out.println("Atleast 1 UpperCase");
+			System.out.println("Atleast 1 Numeric Number");
+	        }
+
 	        
 	    }
 	}
